65c9f838d63965a8624a7756ccd02c6ef72653bf
Glauco <glauco.lorenzut@gmail.com>
2020-09-21 00:43:27 +0200

    Moved graphical stuff in a new file

---
+++ game_GUI.py
@@ -0,0 +1,60 @@
+import pygame
+
+class GameGUI():
+    def __init__(self, screen, board_width, board_height, dimension):
+        self.screen = screen
+        self.width = board_width
+        self.height = board_height
+        self.dimension = dimension
+        self.square_size = board_width // dimension
+        self.images = {}
+
+    def init_board_index(self):
+        index_map = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10}
+
+        for abc, ind in index_map.items():
+            self.write_index(ind + 1, 44, self.width, (10 - ind) * self.square_size)
+            self.write_index(abc, 44, ind * self.square_size, self.height)
+
+    def write_index(self, text, size, pos_X, pos_Y):
+        font = pygame.font.Font(None, size)  # pygame.font.SysFont('Times New Roman', 36)
+        label = font.render(str(text), True, pygame.Color("white"))
+        layout_X = 0.5 * (self.square_size - label.get_rect().width)
+        layout_Y = 0.5 * (self.square_size - label.get_rect().height) + 3
+        self.screen.blit(label, (int(pos_X + layout_X), int(pos_Y + layout_Y)))
+        # print(str(text) + " - " + str(label.get_rect().height) + " - " + str(pos_Y + layout_X))
+
+    def load_images(self, path):
+        pieces = ["sS", "gS", "gF"]
+        for piece in pieces:
+            self.images[piece] = pygame.transform.scale(pygame.image.load(path + "/" + piece + ".png"), (self.square_size, self.square_size))
+
+    def get_board_location(self, position):
+        if 0 <= position[0] < self.width and 0 <= position[1] < self.height:
+            return (position[1] // self.square_size, position[0] // self.square_size)
+        return None
+
+    def highlightSquare(self, row, col, color):
+        pygame.draw.rect(self.screen, color, pygame.Rect(col * self.square_size+1,  row* self.square_size+1, self.square_size-1, self.square_size-1))
+
+
+    def draw_board(self):
+        pygame.draw.rect(self.screen, pygame.Color("blue"), pygame.Rect(0, 0, self.width, self.height))
+        for d in range(self.dimension+1):
+            # rows
+            pygame.draw.line(self.screen, pygame.Color("white"), (0, d*self.square_size), (self.width, d*self.square_size), 1)
+            # cols
+            pygame.draw.line(self.screen, pygame.Color("white"), (d * self.square_size, 0), (d * self.square_size, self.height), 1)
+            if (3 <= d <= 8):
+                # inner rows
+                pygame.draw.line(self.screen, pygame.Color("white"), (3 * self.square_size-1, d * self.square_size), (8 * self.square_size+1, d * self.square_size), 3)
+                # inner cols
+                pygame.draw.line(self.screen, pygame.Color("white"), (d * self.square_size, 3 * self.square_size-1), (d * self.square_size, 8 * self.square_size+1), 3)
+
+
+    def draw_pieces(self, board):
+        for r in range(self.dimension):
+            for c in range(self.dimension):
+                piece = board[r][c]
+                if piece != "--":
+                    self.screen.blit(self.images[piece], pygame.Rect(c*self.square_size, r*self.square_size, self.square_size, self.square_size))
--- main.py
+++ main.py
@@ -1,47 +1,39 @@
 import os
 import pygame
 import game_engine
+import game_GUI
 
 
 IMGS_PATH = "images"
 WINDOW_LAYOUT = (550, 80)
-WINDOW_WIDTH = WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 980
+WINDOW_HEIGHT = 720
 BOARD_WIDTH = BOARD_HEIGHT = 660
 DIMENSION = 11
-
-SQ_SIZE = BOARD_HEIGHT // DIMENSION
 MAX_FPS = 15
-IMAGES = {}
 
 
 def main():
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % WINDOW_LAYOUT
-    #pygame.display.set_caption("asd")
-    pygame.init()
-    pygame.font.init()
 
+    #pygame.display.set_caption("asd")
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     screen.fill(pygame.Color("black"))
 
-    def write_index(text, size, pos_X, pos_Y):
-        font = pygame.font.Font(None, size)  # pygame.font.SysFont('Times New Roman', 36)
-        label = font.render(str(text), True, pygame.Color("white"))
-        layout_X = 0.5*(SQ_SIZE - label.get_rect().width)
-        layout_Y = 0.5*(SQ_SIZE - label.get_rect().height) + 3
-        screen.blit(label, (int(pos_X + layout_X), int(pos_Y + layout_Y)))
-        #print(str(text) + " - " + str(label.get_rect().height) + " - " + str(pos_Y + layout_X))
 
-    index_map = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10}
-    for abc, ind in index_map.items():
-        write_index(ind+1, 44, BOARD_WIDTH, (10-ind)*SQ_SIZE)
-        write_index(abc, 44, ind * SQ_SIZE, BOARD_HEIGHT)
-
+    pygame.init()
+    pygame.font.init()
     clock = pygame.time.Clock()
 
+    gui = game_GUI.GameGUI(screen, BOARD_WIDTH, BOARD_HEIGHT, DIMENSION)
     game = game_engine.GameState()
+
+    gui.init_board_index()
+
     validMoves = game.getAllPossibleMoves()
+
     moveMade = False
-    loadImages()
+    gui.load_images(IMGS_PATH)
     running = True
     sqSelected = ()
     playerClicks = []
@@ -52,14 +44,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                location = pygame.mouse.get_pos()
+                location = gui.get_board_location(pygame.mouse.get_pos())
 
-                if 0 <= location[0] < BOARD_WIDTH and 0 <= location[1] < BOARD_HEIGHT:
-                    row = location[1] // SQ_SIZE
-                    col = location[0] // SQ_SIZE
-                    #print((row, col))
-                    if sqSelected != (row, col) and (game.isValidPiece(row, col) or len(playerClicks) > 0):
-                        sqSelected = (row, col)
+                if location:
+                    row, col = location
+                    if sqSelected != location and (game.isValidPiece(row, col) or len(playerClicks) > 0):
+                        sqSelected = location
                         playerClicks.append(sqSelected)
                     else:
                         sqSelected = ()
@@ -67,15 +57,11 @@ def main():
 
                     if len(playerClicks) == 2:
                         move = game_engine.Move(playerClicks[0], playerClicks[1], game.board)
-                        asd = True
                         for check_move in validMoves:
                             if move == check_move:
                                 game.makeMove(move)
                             else:
                                 print(move.ID + " - " + check_move.ID)
-
-
-
 
                         sqSelected = ()
                         playerClicks = []
@@ -104,50 +90,18 @@ def main():
                 validMoves = game.getAllPossibleMoves()
                 moveMade = False
 
-        drawBoard(screen)
+        gui.draw_board()
 
         if len(playerClicks) > 0:
             if game.isYourTurn(playerClicks[0][0], playerClicks[0][1]):
-                highlightSquare(screen, playerClicks[0][0], playerClicks[0][1], pygame.Color(50, 170, 80))
+                gui.highlightSquare(playerClicks[0][0], playerClicks[0][1], pygame.Color(50, 170, 80))
             else:
-                highlightSquare(screen, playerClicks[0][0], playerClicks[0][1], pygame.Color(170, 50, 80))
+                gui.highlightSquare(playerClicks[0][0], playerClicks[0][1], pygame.Color(170, 50, 80))
 
-        drawPieces(screen, game.board)
+        gui.draw_pieces(game.board)
 
         clock.tick(MAX_FPS)
         pygame.display.flip()
-
-
-def loadImages():
-    pieces = ["sS", "gS", "gF"]
-    for piece in pieces:
-        IMAGES[piece] = pygame.transform.scale(pygame.image.load(IMGS_PATH + "/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
-
-
-def highlightSquare(screen, row, col, color):
-    pygame.draw.rect(screen, color, pygame.Rect(col * SQ_SIZE+1,  row* SQ_SIZE+1, SQ_SIZE-1, SQ_SIZE-1))
-
-
-def drawBoard(screen):
-    pygame.draw.rect(screen, pygame.Color("blue"), pygame.Rect(0, 0, BOARD_WIDTH, BOARD_HEIGHT))
-    for d in range(DIMENSION +1):
-        # rows
-        pygame.draw.line(screen, pygame.Color("white"), (0, d*SQ_SIZE), (BOARD_WIDTH, d*SQ_SIZE), 1)
-        # cols
-        pygame.draw.line(screen, pygame.Color("white"), (d * SQ_SIZE, 0), (d * SQ_SIZE, BOARD_HEIGHT), 1)
-        if (3 <= d <= 8):
-            # inner rows
-            pygame.draw.line(screen, pygame.Color("white"), (3 * SQ_SIZE-1, d * SQ_SIZE), (8 * SQ_SIZE+1, d * SQ_SIZE), 3)
-            # inner cols
-            pygame.draw.line(screen, pygame.Color("white"), (d * SQ_SIZE, 3 * SQ_SIZE-1), (d * SQ_SIZE, 8 * SQ_SIZE+1), 3)
-
-
-def drawPieces(screen, board):
-    for r in range(DIMENSION):
-        for c in range(DIMENSION):
-            piece = board[r][c]
-            if piece != "--":
-                screen.blit(IMAGES[piece], pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == "__main__":
